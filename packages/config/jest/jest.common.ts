b843a061ef29e9ff434f03e90372c458974a2b0b
Maxim Mukhanov <maximmukhanov.job@gmail.com>
2023-05-02 14:07:22 +0300

    style: revamped project config

--- packages/config/jest/jest.common.ts
+++ packages/config/jest/jest.common.ts
@@ -2,7 +2,7 @@ import type { Config } from '@jest/types';
 
 const commonConfig: Config.InitialOptions = {
   preset: 'ts-jest',
-  roots: ['packages/config'],
+  roots: ['<rootDir>/../../packages/config/'],
   setupFiles: ['./setupMock.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
