548fe803b9af620f26f9a6752e199c3680b5ffc7
Maxim Mukhanov <maximmukhanov.job@gmail.com>
2023-05-07 17:23:43 +0300

    added jest to shared

--- packages/shared/.eslintrc.js
+++ packages/shared/.eslintrc.js
@@ -1,16 +1,4 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
+  extends: ['incomer-api'],
 };
---
+++ packages/shared/jest.config.js
@@ -0,0 +1,5 @@
+const config = {
+  preset: [],
+};
+
+export default config;
